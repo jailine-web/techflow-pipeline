4a62af83727b8113ccaaa10b011a47c493b2ff7c
unknown <jay.santana964@gmail.com>
2026-06-18 11:33:05 -0300

    Correções arquivo test_calculator

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,6 +1,6 @@
 import pytest
-
 from src.calculator import Calculator
+
 
 class TestCalculator:
     def setup_method(self):
@@ -9,18 +9,22 @@ class TestCalculator:
     def test_add(self):
         assert self.calc.add(2, 3) == 5
         assert self.calc.add(-1, 1) == 0
+        assert self.calc.add(0, 0) == 0
 
     def test_subtract(self):
         assert self.calc.subtract(10, 3) == 7
         assert self.calc.subtract(0, 5) == -5
+        assert self.calc.subtract(-5, -5) == 0
 
     def test_multiply(self):
         assert self.calc.multiply(4, 3) == 12
         assert self.calc.multiply(0, 10) == 0
+        assert self.calc.multiply(-2, 3) == -6
 
     def test_divide(self):
         assert self.calc.divide(10, 2) == 5
         assert self.calc.divide(7, 2) == 3.5
+        assert self.calc.divide(-10, 2) == -5
 
     def test_divide_by_zero(self):
         with pytest.raises(ValueError):
